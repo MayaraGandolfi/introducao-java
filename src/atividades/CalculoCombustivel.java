11f3e809cc77684baeff4fca4f7f63eefdfd98f9
Mayara <mayaraagandolfi@gmail.com>
2022-09-22 14:27:03 -0300

    Ajuste em atividades

--- src/atividades/CalculoCombustivel.java
+++ src/atividades/CalculoCombustivel.java
@@ -6,11 +6,14 @@ public class CalculoCombustivel {
 
         final double KM_POR_LITRO = 12;
         double velocidade = 60;
-        //variavel de tempo
 
-        //em andamento
-        System.out.println(KM_POR_LITRO);
+        double tempo = 3;
+        double distancia = tempo * velocidade;
+        double litrosUsados = distancia / KM_POR_LITRO;
 
+        System.out.printf("A velocidade média da viagem foi de %.0f Km/hora, " +
+                "durou %.0f horas, com uma distancia de %.2f km e utilizando %.2f litros " +
+                "de gasolina", velocidade, tempo, distancia, litrosUsados);
 
     }
 
