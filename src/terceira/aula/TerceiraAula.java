79e99cce22aacf72fe585ff846a8f93141170b23
Mayara <mayaraagandolfi@gmail.com>
2022-09-26 15:26:45 -0300

    Terceira aula

---
+++ src/terceira/aula/TerceiraAula.java
@@ -0,0 +1,33 @@
+package terceira.aula;
+
+import java.util.Scanner;
+
+public class TerceiraAula {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a linguagem: ");
+        String linguagem = scanner.next();
+
+        linguagem = linguagem.toUpperCase();
+
+        switch (linguagem) {
+            case "JAVA":
+                System.out.println("É uma linguagem compilada.");
+                break;
+            case "KOTLIN":
+                System.out.println("É uma linguagem compilada.");
+                break;
+            case "PHP":
+                System.out.println("É uma linguagem interpretada.");
+                break;
+            case "JAVASCRIPT":
+                System.out.println("É uma linguagem interpretada.");
+                break;
+            default:
+                System.out.println("Linguagem não encontrada.");
+                break;
+        }
+    }
+}
